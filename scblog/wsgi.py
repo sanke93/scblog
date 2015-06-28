25a7c97e7b3eda9e61efaa4c1100be15655de90e
Sanket Chauhan <sanke93@gmail.com>
2015-06-28 14:53:23 -0400

    updated wsgi file

--- scblog/wsgi.py
+++ scblog/wsgi.py
@@ -1,4 +1,4 @@
-"""
+	"""
 WSGI config for scblog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+import sys	
+sys.path.append('~/public_html/sanketchauhan.com/')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scblog.settings")
 
 application = get_wsgi_application()
+
