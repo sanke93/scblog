a7284161172b576fc839db6abc5e2663fbc536a3
Sanket Chauhan <sanke93@gmail.com>
2015-07-30 18:18:36 -0400

    added logging

--- scblog/settings.py
+++ scblog/settings.py
@@ -126,4 +126,29 @@ STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+        }
+    },
+    'handlers': {
+        'gunicorn': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
+            'maxBytes': 1024 * 1024 * 100,  # 100 mb
+        }
+    },
+    'loggers': {
+        'gunicorn.errors': {
+            'level': 'DEBUG',
+            'handlers': ['gunicorn'],
+            'propagate': True,
+        },
+    }
+}
 
