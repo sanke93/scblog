cdc13fd0cb47ae547fb768682c363636cc133bee
Sanket Chauhan <sanke93@gmail.com>
2016-01-31 13:25:37 -0800

    added about page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.BlogIndex.as_view(), name="index"),
     url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail"),
+    #url(r'^about/$', views.BlogAbout.as_view(), name="about"),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+
 )
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 from . import models
+from django.http import HttpResponse
+from django.template import loader
 
 class BlogIndex(generic.ListView):
     queryset = models.Entry.objects.published()
